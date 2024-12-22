120949f9d39e7e03a0707e7100f418c08bbf506f
shankarjatin <shankarjatin1005@gmail.com>
2024-12-22 21:38:18 +0530

    image congfig

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -84,7 +84,7 @@ export default function Home() {
           </Button>
         </div>
 
-        {/* <div className="relative overflow-hidden pt-16">
+        <div className="relative overflow-hidden pt-16">
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <Image
               alt="App screenshot"
@@ -97,7 +97,7 @@ export default function Home() {
               <div className="absolute bottom-0 -inset-x-32 bg-gradient-to-t from-white/95 pt-[5%]" />
             </div>
           </div>
-        </div> */}
+        </div>
 
         <div className="mx-auto mt-16 max-w-7xl px-6 sm:mt-20 md:mt-24 lg:px-8">
           <dl className="mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-10 text-base leading-7 text-gray-600 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16">
